ebcf8c7db72e240c9ad6df467319fee0740e0689
mohamedhisham404 <mohamedhisham694@gmail.com>
2023-08-20 20:26:15 +0300

    some changes on queue and stack with array

--- Stack_with_array.cpp
+++ Stack_with_array.cpp
@@ -1,51 +1,58 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-#define size 5
-int stack[size], top = -1;
-void push(int value);
-void display();
-int peek();
-int pop();
 
-int main() {
-	push(5);
-	push(10);
-	push(15);
-	display();
-	cout << endl;
-	pop();
-	display();
-	cout << endl;
-	cout << "peek=" << peek();
-	return 0;
-}
-void push(int value) {
-	if (top == size - 1)
+template<class T>class Stack{
+private:
+	int sz=100;
+	vector<T>stack=vector<T>(sz);
+	int top = -1;
+
+public:
+	void push(int value) {
+	if (top == sz - 1)
 		cout << "stack over flow";
 	else {
 		top++;
 		stack[top] = value;
 	}
-}
-int pop() {
-	if (top == -1)
-		cout << "stack is under flow";
-	else {
-		return stack[top--];
 	}
-}
-int peek() {
-	if (top == -1)
-		cout << "stack is under flow";
-	else {
-		return stack[top];
+	T pop() {
+		if (top == -1){
+			cout << "stack is under flow";
+			return 0;
+		}
+		else {
+			return stack[top--];
+		}
 	}
-}
-void display() {
-	if (top == -1)
-		cout << "stack is under flow";
-	else {
-		for (int i = top; i >= 0; i--)
-			cout << stack[i] << endl;
+	T peek() {
+		if (top == -1){
+			cout << "stack is under flow";
+			return 0;
+		}
+		else {
+			return stack[top];
+		}
 	}
+	void display() {
+		if (top == -1)
+			cout << "stack is under flow";
+		else {
+			for (int i = top; i >= 0; i--)
+				cout << stack[i] << endl;
+		}
+	}
+};
+int main() {
+	Stack<int>st;
+	st.push(5);
+	st.push(10);
+	st.push(15);
+	st.display();
+	cout << endl;
+	st.pop();
+	st.display();
+	cout << endl;
+	cout << "peek=" << st.peek();
+	return 0;
 }
--- queue_with_array.cpp
+++ queue_with_array.cpp
@@ -1,60 +1,66 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-#define size 5
-int queue[size], front = -1, rear = -1;
 
-void enqueue(int value);
-void dequeue();
-int peek();
-void display();
+template<class T>class Queue{
+public:
+	int front = -1, rear = -1;
+	int sz=100;
+	vector<T>queue=vector<T>(sz);	
+
+	void enqueue(T value) {
+		if (rear == sz - 1)
+			cout << "queue is over flow";
+		else if (front == -1 && rear == -1) {
+			rear++;
+			front++;
+			queue[rear] = value;
+		}
+		else
+			queue[++rear] = value;
+	}
+	void dequeue() {
+		if (front != -1 && rear != -1 && front <= rear) {
+			front++;
+		}
+		else {
+			cout << "is empty";
+		}
+	}
+	T peek() {
+		if (front != -1 && rear != -1 && front <= rear) {
+			return queue[front];
+		}
+		else {
+			cout << "is empty";
+			return -1;
+		}
+	}
+	void display() {
+		if (front != -1 && rear != -1 && front <= rear) {
+			for (int i = front; i <= rear; i++)
+				cout << queue[i] << "\n";
+		}
+		else {
+			cout << "is empty";
+		}
+	}
+
+};
+
+
 
 int main() {
-	enqueue(1);
-	enqueue(2);
-	enqueue(3);
-	enqueue(4);
-	enqueue(5);
-	display();
-	cout << endl;
-	dequeue();
-	display();
-	cout << endl;
-	cout << "peek=" << peek();
-}
-void enqueue(int value) {
-	if (rear == size - 1)
-		cout << "queue is over flow";
-	else if (front == -1 && rear == -1) {
-		rear++;
-		front++;
-		queue[rear] = value;
-	}
-	else
-		queue[++rear] = value;
-}
-void dequeue() {
-	if (front != -1 && rear != -1 && front <= rear) {
-		front++;
-	}
-	else {
-		cout << "is empty";
-	}
-}
-int peek() {
-	if (front != -1 && rear != -1 && front <= rear) {
-		return queue[front];
-	}
-	else {
-		cout << "is empty";
-		return -1;
-	}
-}
-void display() {
-	if (front != -1 && rear != -1 && front <= rear) {
-		for (int i = front; i < rear; i++)
-			cout << queue[i] << "\t";
-	}
-	else {
-		cout << "is empty";
-	}
+	Queue<double>q;
+	q.enqueue(1.1);
+	q.enqueue(2.1);
+	q.enqueue(1.3);
+	q.enqueue(1.6);
+	q.dequeue();
+
+	cout<<q.peek()<<endl;
+
+	q.display();
+	
+
+	return 0;
 }
